983341e0f1d035c9669119d4d8e1bc65a3f20b6f
oscar giordano <gomezgiordanooscar@gmail.com>
2024-12-18 21:45:35 -0300

    versel deploy backend

--- server.js
+++ server.js
@@ -10,8 +10,11 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json({ limit: "10mb" })); // Aumenta el límite del JSON a 10 MB
 app.use(express.urlencoded({ extended: true, limit: "10mb" })); // También para datos codificados en URL
-app.use(express.json()); // Soporte para JSON
-app.use(cors()); // Soporte para solicitudes CORS
+app.use(cors({
+  origin: ["https://gestion-de-barra-frontend.vercel.app"], // Reemplaza con la URL de tu frontend
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+  credentials: true,
+}));
 
 // Rutas
 app.use("/api/pedidos", require("./routes/pedidosRoutes"));
@@ -19,11 +22,16 @@ app.use("/api/premixes", require("./routes/premixesRoutes"));
 app.use("/api/tragos", require("./routes/tragosRoutes"));
 app.use("/api/productos86", require("./routes/productos86Routes"));
 
+// Ruta base para verificar el estado del servidor
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
 // Conexión a MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Conectado a MongoDB"))
-  .catch((err) => console.error("Error al conectar a MongoDB:", err));
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
 // Iniciar el servidor
-app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
